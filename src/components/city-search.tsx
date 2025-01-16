eaf56ad1edc6638126304b527af2a6ab5bc3282e
Sachinleema <sachinleema347@gmail.com>
2025-01-16 17:51:16 +0530

    package json fix

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -1,5 +1,10 @@
 const CitySerach = () => {
-  return <div>CitySerach</div>;
+  return (
+    <input
+      placeholder="Enter city"
+      className="hidden md:block rounded-md px-4 py-2 border mx-4"
+    />
+  );
 };
 
 export default CitySerach;
